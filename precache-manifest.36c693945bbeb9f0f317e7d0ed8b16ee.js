df26fa7b735d8990e8f6557358a269c9e2f31936
Rakesh P <rakeshpanugoth2003@gmail.com>
2022-07-01 00:31:33 +0530

    Updates

---
+++ precache-manifest.36c693945bbeb9f0f317e7d0ed8b16ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9f01d38a3e390021110",
+    "url": "/TextUtils-react/static/css/main.58775d9b.chunk.css"
+  },
+  {
+    "revision": "e9f01d38a3e390021110",
+    "url": "/TextUtils-react/static/js/main.e9f01d38.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils-react/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "79c0a2055b2b445bed0a",
+    "url": "/TextUtils-react/static/js/runtime~main.79c0a205.js"
+  },
+  {
+    "revision": "6763ef4b33ef65a7d94e2455841c2326",
+    "url": "/TextUtils-react/index.html"
+  }
+];
